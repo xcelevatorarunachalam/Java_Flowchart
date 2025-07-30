b33cb53fac99a0f8f9f04da6f5322925f0662db4
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-30 10:00:06 +0530

    print quotient and reminder

---
+++ FCP005.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class FCP005{
+public static void main(String a[]){
+Scanner s= new Scanner(System.in);
+int x=s.nextInt();
+int y=s.nextInt();
+System.out.println(x/y);
+System.out.println(x%y);}}
