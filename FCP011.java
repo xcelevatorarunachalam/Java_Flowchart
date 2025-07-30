8c1c704ce51ebdb0c66dafc3c75962326870e64f
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-30 13:00:11 +0530

    Odd or Even Check

---
+++ FCP011.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FCP011{
+public static void main(String a[]){
+Scanner s=new Scanner(System.in);
+int x=s.nextInt();
+if(x%2==0){
+System.out.println("even");}
+else
+{
+System.out.println("odd");
+}}}
