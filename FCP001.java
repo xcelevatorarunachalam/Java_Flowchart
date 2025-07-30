9cc7c5500a8bc3b13d57138dcc798e0e4a52c595
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-30 09:54:23 +0530

    print DOB in order

---
+++ FCP001.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FCP001{
+public static void main(String a[])
+{
+Scanner s=new Scanner(System.in);
+int day=s.nextInt();
+s.nextLine();
+String month=s.nextLine();
+int year=s.nextInt();
+System.out.println(day);
+System.out.println(month);
+System.out.println(year);}
+}
