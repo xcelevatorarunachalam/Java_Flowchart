a00cc944b6b127d7687c23a627f9fff05f771573
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-30 12:07:38 +0530

    max of two numbers

---
+++ FCP010.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FCP010{
+public static void main(String a[]){
+Scanner s=new Scanner(System.in);
+int x=s.nextInt();
+int y=s.nextInt();
+if(x>y){
+System.out.println(x);
+}
+else{
+System.out.println(y);}}}
