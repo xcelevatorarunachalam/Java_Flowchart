32ee68f2277a1c0da28202d4ec9b5631bfb7ccbe
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-30 10:15:03 +0530

    Celsius to Fahrenheit Conversion

---
+++ FCP007.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class FCP007{
+public static void main(String a[]){
+Scanner s=new Scanner(System.in);
+int c=s.nextInt();
+System.out.println((float)((float)(c*9)/5)+32);}}
