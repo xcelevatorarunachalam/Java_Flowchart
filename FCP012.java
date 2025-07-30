792015c4c0a34147268469cd4cf5202b491c29b4
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-30 15:58:23 +0530

    Positive, Negative or Zero

---
+++ FCP012.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FCP012{
+public static void main(String a[])
+{
+Scanner s=new Scanner(System.in);
+int x=-s.nextInt();
+if(x<0)
+{System.out.println("positive");
+}
+else if(x==0){
+System.out.println("zero");
+}
+else{System.out.println("negative");
+}}}
