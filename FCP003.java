4a5da44007ce4936999facf31bedd2fa08027a30
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-30 09:58:53 +0530

    swap with temp

---
+++ FCP003.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FCP003{
+public static void main(String a[]){
+Scanner s=new Scanner(System.in);
+int b=s.nextInt();
+int c=s.nextInt();
+int temp=b;
+b=c;
+c=temp;
+System.out.println(b);
+System.out.println(c);}}
