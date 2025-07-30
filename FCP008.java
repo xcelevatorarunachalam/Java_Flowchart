430ea35d1578b810f4b2fe9199b19ee7d9422c77
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-30 10:20:00 +0530

    Swap Without Temp

---
+++ FCP008.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FCP008{
+public static void main(String a[]){
+Scanner s=new Scanner(System.in);
+int x=s.nextInt();
+int y=s.nextInt();
+x=x+y;
+y=x-y;
+x=x-y;
+System.out.println(x);
+System.out.println(y);
+}}
