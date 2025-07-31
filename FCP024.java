535e9ca2281bc5d4afcd11186174690d59f4d08d
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-31 14:41:55 +0530

    print even numbers upto N

---
+++ FCP024.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FCP024{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=0;i<n;i++){
+if(i%2==0){
+System.out.println(i);
+}
+}}}
