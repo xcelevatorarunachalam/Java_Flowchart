23d0142a17369390b02906d1eb34c2bbd99c6470
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-07-31 14:34:48 +0530

    Print the user input n times

---
+++ FCP023.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FCP023{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+s.nextLine();
+String user=s.nextLine();
+for(int i=0;i<n;i++){
+System.out.println(user);
+}}}
