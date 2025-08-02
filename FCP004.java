032fedf898fb90ff51d177ab3630044dc88f2c3e
xcelevatorarunachalam <arunachalam.2504@xcelevate.org>
2025-08-02 16:01:05 +0530

    Basic Arithmatic Ops

---
+++ FCP004.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FCP004{
+public static void main(String a[])
+{
+Scanner s=new Scanner(System.in);
+float b=s.nextInt();
+int c=s.nextInt();
+System.out.println((int)b+c);
+System.out.println((int) b-c);
+System.out.println((int)b*c);
+float d=(b/c);
+System.out.println(d);
+System.out.println((int)b/c);
+System.out.println((int)b%c);}}
